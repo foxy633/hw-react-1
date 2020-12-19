e7e685b6a42b51d844131234735338b3f4d1b93b
Anastasiya Teslenko <tislenko633@gmail.com>
2020-12-19 16:49:19 +0200

    addHw-1-1.0

---
+++ src/components/FriendList/FriendList.js
@@ -0,0 +1,32 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styles from "./friendsList.module.css";
+
+const FriendList = ({ friends }) => (
+  <ul className={styles.friend}>
+    {friends.map((friend) => (
+      <li className={styles.item} key={friend.id}>
+        <span
+          className={
+            friend.isOnline ? styles.statusOnline : styles.statusOffline
+          }
+        ></span>
+        <img className={styles.avatar} src={friend.avatar} alt="" width="50" />
+        <p className={styles.name}>{friend.name}</p>
+      </li>
+    ))}
+  </ul>
+);
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      avatar: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      isOnline: PropTypes.bool.isRequired,
+    })
+  ),
+};
+
+export default FriendList;
